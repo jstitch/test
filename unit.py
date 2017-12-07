0a9d7ee830d07c99f3b6421d7ef92363c29deabc
Javier Novoa C <jstitch@gmail.com>
2017-12-06 21:39:38 -0600

    fallando

--- unit.py
+++ unit.py
@@ -4,6 +4,7 @@ import test
 class Pruebas(unittest.TestCase):
     def test_concat(self):
         self.assertEqual(test.suma("hola","mundo"), "holamundo")
+        self.fail("fallando :v")
 
 if __name__=="__main__":
     unittest.main()
